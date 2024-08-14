2ef309e17ff60ed8331274a2d1e604be02a0f041
nicholasxjy <nicholasxjy@gmail.com>
2024-08-14 14:58:04 +0800

    fix conform in lsp servers

--- lua/lualine/components/fancy_lsp_servers.lua
+++ lua/lualine/components/fancy_lsp_servers.lua
@@ -17,24 +17,26 @@ function M:update_status()
 	local null_ls_installed, null_ls = pcall(require, "null-ls")
 	local conform_installed, conform = pcall(require, "conform")
 	local buf_client_names = {}
+
 	for _, client in pairs(buf_clients) do
-		if client.name == "null-ls" then
-			if null_ls_installed then
-				for _, source in ipairs(null_ls.get_source({ filetype = vim.bo.filetype })) do
-					table.insert(buf_client_names, source.name)
-				end
-			end
-		elseif client.name == "conform" then
-			if conform_installed then
-				local formatters = conform.list_formatters(0)
-				for _, source in ipairs(formatters) do
-					table.insert(buf_client_names, source.name)
-				end
-			end
-		else
+		if client.name ~= "null-ls" then
 			table.insert(buf_client_names, client.name)
 		end
 	end
+
+	if null_ls_installed then
+		for _, source in ipairs(null_ls.get_source({ filetype = vim.bo.filetype })) do
+			table.insert(buf_client_names, source.name)
+		end
+	end
+
+	if conform_installed then
+		local formatters = conform.list_formatters(0)
+		for _, source in ipairs(formatters) do
+			table.insert(buf_client_names, source.name)
+		end
+	end
+
 	return table.concat(buf_client_names, self.options.split)
 end
 
